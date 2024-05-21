73daad8ecd2147eafee5aa80a433b422a5ba1a54
Rownak <rownakkabit007@gmail.com>
2024-05-21 15:16:57 +0600

    first commit

---
+++ src/Class1/Identifiers.java
@@ -0,0 +1,22 @@
+package Class1;
+
+public class Identifiers {
+    public static void main(String[] args) {
+        int Stu_ID = 5;
+        char Grade = 'A';
+        float CGP = 5.75f;
+        String $name="Rahul";
+
+
+        System.out.println(Stu_ID);
+        System.out.println(Grade);
+        System.out.println(CGP);
+    }
+    public void Print_Result(){
+
+
+
+
+    }
+
+    }
